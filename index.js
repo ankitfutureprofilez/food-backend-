24dc1e07dec5811262ba5ac0c2ee61fc1fbfcf0d
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-02-07 13:08:27 +0530

    aa

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ dotenv.config();
 //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 //   credentials: true, 
 // };
-// app.use(cors({
-//   origin: ["https://food-backend-one.vercel.app/"],
-//   methods: ["POST", "GET", "DELETE", "PUT"],
-//   credentials: true
-// }));
+app.use(cors({
+  origin: ["https://food-backend-one.vercel.app"],
+  methods: ["POST", "GET", "DELETE", "PUT"],
+  credentials: true
+}));
 
-app.use(cors())
+// app.use(cors())
 // app.use(cors(corsOptions));
 app.use(express.json({ limit: "10mb" }));
 
