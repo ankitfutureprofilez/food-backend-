232da72005aa25cd739d53e8ce491b792b2c5d7d
sunil-sainiibs37 <sunil-sainiibs37@example.com>
2024-02-09 12:54:25 +0530

    backend

--- index.js
+++ index.js
@@ -8,39 +8,25 @@ const corsOptions = {
   credentials: true, 
 };
 app.use(cors(corsOptions));
-
 const dotenv = require("dotenv");
 require("./mongoConfig");
 dotenv.config();
-
 const morgan = require('morgan')
 app.use(morgan('dev')); 
-
-app.use(cors({
-  origin: "*", 
-  credentials: true, 
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "mode"] // Allow additional headers, including "mode"
-}));
-
-
 app.use(express.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
-
-
 app.use("/product", require("./routes/productRoutes"));
 app.use("/user", require("./routes/userRoutes"));
 app.use("/restaurant", require("./routes/restaurantRoutes"));
 app.use("/stripe", require("./routes/stripeRoutes"));
 
 const PORT = process.env.REACT_APP_SERVER_DOMIN;
-
+console.log("PORT",PORT)
 app.get("/", (req, res) => {
   res.json({
     msg:'Okay',
     status:200
   })
 })
-
-
 app.listen(PORT, () => console.log("Server is running at port : " + PORT));
  
