361fe2fd55eb905c900c6f45597127d48d0af524
Naveen Tehrpariya <naveen@internetbusinesssolutionsindia.com>
2024-02-09 15:38:04 +0530

    latest changes added

--- db/Product.js
+++ db/Product.js
@@ -5,11 +5,7 @@ const productSchema = mongoose.Schema({
   image: String,
   price: String,
   description: String,
-<<<<<<< HEAD
-  userId:String
-=======
   userId: String
->>>>>>> d6c7cb7c418f40d39039c07bb4a9caf4a8688fa3
 });
 
 productSchema.virtual('permalink').get(function() {
