c4756795bc613e25b67980feb6e72dc1871988f5
Naveen Tehrpariya <naveen@internetbusinesssolutionsindia.com>
2024-02-09 15:45:13 +0530

    latest changes added

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-var bodyParser =  require("body-parser")
 const cors = require("cors");
 const corsOptions = {
   origin: 'http://localhost:3000',
