7a3daf0620e5cda20681a8ad70b7e6b1721deb18
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-02-13 10:42:21 +0530

    lofi

--- routes/stripeRoutes.js
+++ routes/stripeRoutes.js
@@ -2,6 +2,7 @@ const route = require("express").Router();
 const { validateToken } = require("../controller/AuthController");
 const { createCheckout } = require("../controller/stripeController");
 
+
 route.post("/create-checkout-session", validateToken, createCheckout);
 
 module.exports = route;
