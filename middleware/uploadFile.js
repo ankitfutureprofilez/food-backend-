20f701603310a41a3c2cd7de9435f49cb5426218
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-02-14 11:38:23 +0530

    chenagement

--- middleware/uploadFile.js
+++ middleware/uploadFile.js
@@ -1,7 +1,7 @@
 const multer = require('multer');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, 'uploads'))
+    cb(null, path.join(__dirname, 'uploads')) 
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname);
