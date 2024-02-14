2db508ecd69470e43160dc42d2e49c43f8163e2f
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-02-14 09:42:38 +0530

    changemetn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const corsOptions = {
-  origin: 'https://food-delivery-websites.netlify.app',
+  origin: 'http://localhost:3000',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, 
 };
