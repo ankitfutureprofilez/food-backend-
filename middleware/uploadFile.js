0db05b7d09361ab6bc9edf5ee7a331d7a01a4203
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-02-14 11:27:14 +0530

    add image

--- middleware/uploadFile.js
+++ middleware/uploadFile.js
@@ -1,7 +1,7 @@
 const multer = require('multer');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'storage/');
+    cb(null, '/storage/');
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname);
