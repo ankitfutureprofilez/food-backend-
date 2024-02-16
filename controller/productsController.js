799db96e73df5267084189e8abbf07171ccf25a0
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-02-16 13:29:36 +0530

    data

--- controller/productsController.js
+++ controller/productsController.js
@@ -39,7 +39,8 @@ exports.addProduct = catchAsync(async (req, res) => {
 
 exports.productlist = catchAsync (
   async (req, res) => {
-    const record = await product.find({}).populate('rest').exec();
+    const record = await product.find({}).populate('userId').exec();
+    console.log("record" ,record)
     res.json({
       data: record,
       status: 200,
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -12,11 +12,6 @@ routes.post("/uploadProduct", validateToken, addProduct);
 routes.get("/productlist", productlist);
 
 
-// resturent detail api - done 
-routes.get("/restaurent/:res_id",  restaurantProducts);
-
-// only my added products - 
-routes.get("/my-products/:userId", validateToken, userproductlist);
 
 
 
