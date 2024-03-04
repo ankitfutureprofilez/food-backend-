23972ad7c7274695cfd1afb171c94988ee2a5176
Abhinav-FP <a.mathur@futureprofilez.com>
2024-03-04 15:27:54 +0530

    Login logic Corrected

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -6,134 +6,7 @@ const AppError = require("../utils/AppError");
 const SECRET_ACCESS = process.env && process.env.SECRET_ACCESS;
 const key = process && process.env && process.env.SECRET_ACCESS;
 
-// Email template
-// const  PasswordResetEmail  = `
-//     <html lang="en-US">
-//       <head>
-//         <meta content="text/html; charset=utf-8" httpEquiv="Content-Type" />
-//         <meta name="description" content="Reset Password Email Template." />
-//         <style>
-//         </style>
-//       </head>
-//       <body style={{ margin: '0px', backgroundColor: '#f2f3f8' }}>
-//         <table
-//           cellSpacing="0"
-//           border="0"
-//           cellPadding="0"
-//           width="100%"
-//           bgcolor="#f2f3f8"
-//           style={{
-//             fontFamily: "'Open Sans', sans-serif",
-//           }}
-//         >
-//           <tr>
-//             <td>
-//               <table
-//                 style={{
-//                   backgroundColor: '#f2f3f8',
-//                   maxWidth: '670px',
-//                   margin: '0 auto',
-//                 }}
-//                 width="100%"
-//                 border="0"
-//                 align="center"
-//                 cellPadding="0"
-//                 cellSpacing="0"
-//               >
-//                 <tr>
-//                   <td style={{ height: '80px' }}>&nbsp;</td>
-//                 </tr>
-//                 <tr>
-//                   <td style={{ textAlign: 'center' }}>
-//                     <a href="https://food-fp.netlify.app/" title="logo" target="_blank">
-//                       <img width="60" src="https://food-fp.netlify.app/logo.png" title="logo" alt="logo" />
-//                     </a>
-//                   </td>
-//                 </tr>
-//                 <tr>
-//                   <td style={{ height: '20px' }}>&nbsp;</td>
-//                 </tr>
-//                 <tr>
-//                   <td>
-//                     <table
-//                       width="95%"
-//                       border="0"
-//                       align="center"
-//                       cellPadding="0"
-//                       cellSpacing="0"
-//                       style={{
-//                         maxWidth: '670px',
-//                         backgroundColor: '#fff',
-//                         borderRadius: '3px',
-//                         textAlign: 'center',
-//                         boxShadow: '0 6px 18px 0 rgba(0,0,0,.06)',
-//                       }}
-//                     >
-//                       <tr>
-//                         <td style={{ height: '40px' }}>&nbsp;</td>
-//                       </tr>
-//                       <tr>
-//                         <td style={{ padding: '0 35px' }}>
-//                           <h1 style={{ color: '#1e1e2d', fontWeight: 500, margin: '0', fontSize: '32px', fontFamily: 'Rubik, sans-serif' }}>
-//                            ${name} You have requested to reset your password
-//                           </h1>
-//                           <span
-//                             style={{
-//                               display: 'inline-block',
-//                               verticalAlign: 'middle',
-//                               margin: '29px 0 26px',
-//                               borderBottom: '1px solid #cecece',
-//                               width: '100px',
-//                             }}
-//                           />
-//                           <p style={{ color: '#455056', fontSize: '15px', lineHeight: '24px', margin: '0' }}>
-//                            OTP for password change is ${Code}. We cannot simply send you your old password.A unique link to reset your password has been generated for you. To reset your password, click the following link and follow the instructions.
-//                           </p>
-//                           <a
-//                             href="https://food-fp.netlify.app/"
-//                             style={{
-//                               background: '#20e277',
-//                               textDecoration: 'none',
-//                               fontWeight: '500',
-//                               marginTop: '35px',
-//                               color: '#fff',
-//                               textTransform: 'uppercase',
-//                               fontSize: '14px',
-//                               padding: '10px 24px',
-//                               display: 'inline-block',
-//                               borderRadius: '50px',
-//                             }}
-//                           >
-//                             Reset Password
-//                           </a>
-//                         </td>
-//                       </tr>
-//                       <tr>
-//                         <td style={{ height: '40px' }}>&nbsp;</td>
-//                       </tr>
-//                     </table>
-//                   </td>
-//                 </tr>
-//                 <tr>
-//                   <td style={{ height: '20px' }}>&nbsp;</td>
-//                 </tr>
-//                 <tr>
-//                   <td style={{ textAlign: 'center' }}>
-//                     <p style={{ fontSize: '14px', color: 'rgba(69, 80, 86, 0.7411764705882353)', lineHeight: '18px', margin: '0 0 0' }}>
-//                       &copy; <strong>https://food-fp.netlify.app/</strong>
-//                     </p>
-//                   </td>
-//                 </tr>
-//                 <tr>
-//                   <td style={{ height: '80px' }}>&nbsp;</td>
-//                 </tr>
-//               </table>
-//             </td>
-//           </tr>
-//         </table>
-//       </body>
-//     </html>
-//     `;
+
 
 // Email logic
 const nodemailer = require("nodemailer");
@@ -219,8 +92,8 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError("Email and password is required !!", 401));
   }
   const user = await User.findOne({ email: email });
-  const isPassword = await User.findOne({ password: password });
-  if (!user || !isPassword) {
+  // const isPassword = await User.findOne({ password: password });
+  if (!user && user.password !==  password ) {
     res.json({
       status: false,
       message: "Invalid Email or password",
